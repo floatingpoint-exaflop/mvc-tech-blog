30cc5e65c7e647a2c22bf45e05dd1b9f1a31d3e8
Frederico Swarthings <timscallon1@gmail.com>
2024-08-19 21:47:34 -0500

    more work on file cleanup and updates

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Comment extends Model {}
 
-Blog.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,11 +11,7 @@ Blog.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    blog_text: {
+    comment_text: {
       type: DataTypes.STRING,
     },
     date_created: {
@@ -23,9 +19,12 @@ Blog.init(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
     },
     user_id: {
       type: DataTypes.INTEGER,
@@ -40,8 +39,8 @@ Blog.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'blog',
+    modelName: 'comment',
   }
 );
 
-module.exports = Blog;
+module.exports = Comment;
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#blog-name').value.trim();
+  const title = document.querySelector('#blog-title').value.trim();
   const needed_funding = document.querySelector('#blog-funding').value.trim();
-  const description = document.querySelector('#blog-desc').value.trim();
+  const blog_text = document.querySelector('#blog-text').value.trim();
 
-  if (name && needed_funding && description) {
+  if (title && needed_funding && blog_text) {
     const response = await fetch(`/api/blog`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, needed_funding, blog_text }),
       headers: {
         'Content-Type': 'application/json',
       },
